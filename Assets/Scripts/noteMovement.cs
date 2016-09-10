ad2ce8742256e01f6f18b6eb7566f0c43cf7ec9b
mahapsub <smahapat@trinity.local>
2016-09-10 12:30:34 -0500

    change1

---
+++ Assets/Scripts/noteMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class noteMovement : MonoBehaviour {
+    public float scrollspeed = 0.05f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = new Vector2(transform.position.x, transform.position.y-scrollspeed/5);
+
+	}
+}
