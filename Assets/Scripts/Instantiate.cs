d9077e57c4b6bddb315b77210678bb77f423e4b1
mmaryyoung <jyang@trinity.local>
2016-09-10 19:52:13 -0500

    A working, simple 4 track model before I screw anything up
    I tried our 3D idea and it is terrible: super messy and not playable at
    all. Also the ends of the four tracks intersected in a super ugly way..
    just hot mess in general. So I swtiched to boring 2D and horizontal
    layout. It's stereotyped but safe..decide to save this first before I
    try anything else that will screw it up.

--- Assets/Scripts/Instantiate.cs
+++ Assets/Scripts/Instantiate.cs
@@ -8,6 +8,7 @@ public class Instantiate : MonoBehaviour
     public float bps = 1;
     public float offset = 0;
     bool beats_bool = true;
+    public GameObject parentTrack;
     // Use this for initialization
     void Start()
     {
@@ -34,7 +35,9 @@ public class Instantiate : MonoBehaviour
         while (beats_bool)
         {
             yield return new WaitForSeconds(waitTime);
-            Object.Instantiate(Resources.Load("Prefabs/noteBlock"), new Vector3(0, 7, 0), Quaternion.identity);
+            //Object.Instantiate(Resources.Load("Prefabs/noteBlock"), new Vector3(0, 7, 0), Quaternion.identity);
+            GameObject newNoteBlock = (GameObject)Instantiate(Resources.Load("Prefabs/noteBlock"), parentTrack.transform);
+            newNoteBlock.transform.localPosition = new Vector3(0, 0, -0.9f);
         }
 
     }
